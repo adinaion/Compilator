19453622ee252e830782cb2894829c0b227b3892
adinaion <79267944+adinaion@users.noreply.github.com>
2024-12-17 23:23:00 +0200

    Add files via upload

---
+++ Compilator/Compilator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using System.Collections.Generic;
+
+public class SpeakLine
+{
+    public string Person { get; set; }
+    public string Text { get; set; }
+}
+
+public class BasicSpeakVisitor : SpeakBaseVisitor<object>
+{
+    public List<SpeakLine> SpeakLines = new List<SpeakLine>();
+
+    public override object VisitLine([NotNull] SpeakParser.LineContext context)
+    {
+        var name = context.name();
+        var opinion = context.opinion();
+
+        SpeakLine line = new SpeakLine() { Person = name.GetText(), Text = opinion.GetText().Trim('"') };
+
+        SpeakLines.Add(line);
+
+        return line;
+    }
+}
+
+public class Program
+{
+    private static SpeakParser Setup(string text)
+    {
+        AntlrInputStream inputStream = new AntlrInputStream(text);
+        SpeakLexer speakLexer = new SpeakLexer(inputStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(speakLexer);
+        SpeakParser speakParser = new SpeakParser(commonTokenStream);
+        return speakParser;
+    }
+
+    public static void Main()
+    {
+        string text = File.ReadAllText("../../../text.in");
+
+        SpeakParser parser = Setup(text);
+        SpeakParser.ChatContext context = parser.chat();
+        BasicSpeakVisitor visitor = new BasicSpeakVisitor();
+        visitor.Visit(context);
+
+        foreach (var line in visitor.SpeakLines)
+        {
+            Console.WriteLine("{0} has said {1}", line.Person, line.Text);
+        }
+    }
+}
---
+++ Compilator/Compilator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Compilator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Compilator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("504be023-8e8a-4431-be68-e668b01e0a4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
